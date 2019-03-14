b314dd0ac79cdd53ff671fbf5eca9146c9deccbe
aguilamotera <pedro.huarez@gmail.com>
2019-03-14 11:48:03 +0100

    Cambios

--- estructurasdinamicas/listasLineales.py
+++ estructurasdinamicas/listasLineales.py
@@ -1,21 +1,46 @@
 import gc
 
-class S:
+class Elemento:
     def __init__(self):
         self.dato = 0 # int
-        self.siguiente=None # Siguiente elemento.
+        self.siguiente=None # Elemento.
 
 def nuevoElemento():
-    return S()
+    return Elemento()
+
+def error():
+    pass
+
+def aniadirAlPrincipio(n, lista):
+    p = lista
+    q = nuevoElemento()
+    q.dato = n
+    q.siguiente = p
+    p = q
+    lista = p
+    return lista
+
+def mostrarTodos(p):
+    q = p
+    while q != None:
+        print(str(q.dato))
+        q = q.siguiente
+
+def borrarTodos(p):
+    q = None
+    while p != None:
+        q = p
+        p = p.siguiente
+        gc.collect
+    return p
+
 
 def main():
-    p = None
-    q = None
-    n = 0
     repetir = True
+    listaLinealSimpleEnlazada = None
+    n = 0
 
     print("Introducir datos.")
-
     while repetir:
         n = input("dato: ")
         
@@ -24,16 +49,12 @@ def main():
         else:
             try:
                 n = int(n)
-
-                q = nuevoElemento()
-                q.dato = n
-                q.siguiente = p
-                p = q
+                listaLinealSimpleEnlazada = aniadirAlPrincipio(n, listaLinealSimpleEnlazada)
             except:
                 print("No es un entero.")
             #finally:
-           
     
-    print("terminado")
-
+    mostrarTodos(listaLinealSimpleEnlazada)
+    listaLinealSimpleEnlazada = borrarTodos(listaLinealSimpleEnlazada)
+    #mostrarTodos(listaLinealSimpleEnlazada)
 main()
---
+++ estructurasdinamicas/pasoPorRef.py
@@ -0,0 +1,12 @@
+
+def funcA(var):
+    var = "A"
+
+def funcB(var):
+    pass
+    
+def main():
+    x = "Hola"
+    funcA(x)
+    print(x)
+main()
